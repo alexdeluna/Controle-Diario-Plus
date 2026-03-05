a080eb8e7c1668d3905eac77a5fd8e983da39ccb
Alex de Luna <67716472+alexdeluna@users.noreply.github.com>
2026-03-05 08:12:55 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'controle-diario-v3';
+const CACHE_NAME = 'controle-diario-v4';
 
 const FILES_TO_CACHE = [
   './',
@@ -43,4 +43,5 @@ self.addEventListener('fetch', event => {
   );
 
 });
+
 
